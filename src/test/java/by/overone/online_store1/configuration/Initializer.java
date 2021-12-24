76f5019615de15d2914ea0aba9ad1572504a1469
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-12-24 22:09:13 +0300

    24.12.2021(22:08)

---
+++ src/test/java/by/overone/online_store1/configuration/Initializer.java
@@ -0,0 +1,24 @@
+package by.overone.online_store1.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/test/java/by/overone/online_store1/controller/UserController.java
@@ -0,0 +1,30 @@
+package by.overone.online_store1.controller;
+
+import by.overone.online_store1.dto.UserDTO;
+import by.overone.online_store1.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+//@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserDTO> readAll(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/hello")
+    public String read(){
+        return "hello";
+    }
+}
